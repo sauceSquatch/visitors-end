47266da3e4c188f52565e26024c1b5450865ce09
Jamie Naioti <naioti@gmail.com>
2021-11-22 16:04:16 -0600

    [ Cash Grain Link ]

---
+++ components/CashGrainLink.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="cash-grain-link">
+    <NuxtLink
+      v-if="!linkExternal"
+      :to="chapterLink"
+    >
+      <div class="arrow-container">
+        <img
+          class="dotted-arrow"
+          src="images/cash_grain/dottedArrow-right.svg">
+      </div>
+      {{ chapterLinkCTA }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    chapterLink: {
+      type: String,
+      required: false,
+    },
+    linkExternal: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    chapterLinkCTA: {
+      type: String,
+      required: false,
+    },
+  },
+  mounted() {
+    //
+  },
+}
+</script>
+
+<style lang="scss">
+  .cash-grain-link {
+    margin: 16px 0;
+    color: $color--brand-white;
+    font-size: 12px;
+    line-height: 1;
+
+    @media (min-width: $break-sm) {
+      font-size: 14px;
+    }
+
+    a, .nuxt-link {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+    }
+
+    a &:visited, a &:-webkit-any-link,
+    .nuxt-link-exact-active, .nuxt-link-active {
+      color: $color--brand-white;
+    }
+
+
+    .arrow-container {
+      display: flex;
+      width: 26px;
+      height: 26px;
+      border-radius: 50%;
+      background-color: $color--brand-orange;
+      justify-content: center;
+      margin-right: 12px;
+      @media (min-width: $break-sm) {
+        width: 40px;
+        height: 40px;
+      }
+
+      .dotted-arrow {
+        width: 22px;
+        @media (min-width: $break-sm) {
+          width: 26px;
+        }
+      }
+    }
+
+  }
+
+</style>
